5ca69021e922250b20fe94afc8ccc7082a901e8b
Oală Oleg <144578970+OlegColeg@users.noreply.github.com>
2025-04-09 13:55:30 +0300

    Corection task 1 add a varible Fahrenheit  and start do task 4

--- Task1/app.js
+++ Task1/app.js
@@ -1,5 +1,6 @@
 function celsiusToFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
+    let Fahrenheit = (celsius * 9 / 5) + 32;
+    return Fahrenheit
 }
 console.log(celsiusToFahrenheit(0))  // Output: 32
 
---
+++ Task4/app.js
@@ -0,0 +1,5 @@
+function countOccurrences(arr, value) { ... }
+
+
+countOccurrences([1, 2, 2, 3, 2, 4], 2);   // Output: 3
+countOccurrences(['a', 'b', 'a', 'c'], 'a'); // Output: 2
