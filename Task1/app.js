7d117d24ca707665f78a02b731438aa5c197130f
Oală Oleg <144578970+OlegColeg@users.noreply.github.com>
2025-04-09 13:40:41 +0300

    Task 2 Add isPrime function to determine if a number is prime

--- Task1/app.js
+++ Task1/app.js
@@ -1,6 +1,6 @@
 function celsiusToFahrenheit(celsius) {
-    return (celsius * 9/5) + 32;
-  }
-  console.log(celsiusToFahrenheit(0))  // Output: 32
+    return (celsius * 9 / 5) + 32;
+}
+console.log(celsiusToFahrenheit(0))  // Output: 32
 
-  console.log(celsiusToFahrenheit(25));   // Output: 77
+console.log(celsiusToFahrenheit(25));   // Output: 77
