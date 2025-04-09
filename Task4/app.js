2303084f40c6108b94b06ebb889556f22a4605c5
Oală Oleg <144578970+OlegColeg@users.noreply.github.com>
2025-04-09 13:59:24 +0300

    Task 4 finish do this task

--- Task4/app.js
+++ Task4/app.js
@@ -1,5 +1,14 @@
-function countOccurrences(arr, value) { ... }
+function countOccurrences(arr, value) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            count++;
+        }
+
+    }
+    return count;
+}
 
 
-countOccurrences([1, 2, 2, 3, 2, 4], 2);   // Output: 3
-countOccurrences(['a', 'b', 'a', 'c'], 'a'); // Output: 2
+console.log(countOccurrences([1, 2, 2, 3, 2, 4], 2)); // Output: 3
+console.log(countOccurrences(['a', 'b', 'a', 'c'], 'a')); // Output: 2
